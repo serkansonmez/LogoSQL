8f9fd2c71aa7ae82f606e6415aed7d7f57aa4dd7
SERKAN SÖNMEZ <serkansonmez16@hotmail.com>
2024-08-31 14:27:27 +0300

    Add files via upload

---
+++ Gunluk/2024/Altincezve dovizkurlari Id update işlemi.sql
@@ -0,0 +1,27 @@
+USE [AltinCezveB2B_Default_v1]
+GO
+
+SELECT [Id]
+      ,[Tarih]
+      ,[CrossOrder]
+      ,[Kod]
+      ,[CurrencyCode]
+      ,[UNIT]
+      ,[Isim]
+      ,[CurrencyName]
+      ,[ForexBuying]
+      ,[ForexSelling]
+      ,[BanknoteBuying]
+      ,[BanknoteSelling]
+  FROM [dbo].[DovizKurlari]
+
+GO
+WITH CTE AS (
+    SELECT 
+        ROW_NUMBER() OVER (ORDER BY Tarih, CrossOrder) AS NewId,
+        [Id]
+    FROM [dbo].[DovizKurlari]
+)
+UPDATE CTE
+SET Id = NewId;
+
